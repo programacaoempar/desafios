cfbd5b0afe213d436536dba331c859fa6b49d8f2
KarinaPadilha2512 <kdpadilha@gmail.com>
2022-06-08 19:22:08 -0300

    Inserindo teste e ajuste na classe.

--- src/main/java/desafios/FilterNumber.java
+++ src/main/java/desafios/FilterNumber.java
@@ -7,22 +7,7 @@ import java.util.stream.Collectors;
 
 public class FilterNumber {
 
-	public static void main(String[] args) {
-		String[] newList = new String[] { "123", "Diego", "Karina", "34", "Leon", "154", "25/12", "PairProgramming", "17.01" };
-
-		System.out.println( "O tamanho do array é: " + newList.length );
-
-		List<String> stringList = asList( newList );
-		List<String> result = stringList.stream()
-				.filter( value -> validateIsNumber( value ) )
-				.collect( Collectors.toList() );
-
-		System.out.println( "O tamanho do array é: " + result.toArray().length );
-
-		System.out.println( "O tamanho do array é: " + result );
-	}
-
-	private static boolean validateIsNumber(String value) {
+	private boolean validateIsNumber(String value) {
 		try {
 			Integer.parseInt( value );
 
@@ -32,6 +17,13 @@ public class FilterNumber {
 		return true;
 	}
 
+	public List<String> filtra(String[] newList) {
+		List<String> stringList = asList( newList );
+		List<String> result = stringList.stream()
+				.filter( value -> validateIsNumber( value ) )
+				.collect( Collectors.toList() );
+		return result;
+	}
 }
 
 
